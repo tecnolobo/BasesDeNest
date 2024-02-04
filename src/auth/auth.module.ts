9517b3d84a3bee782b0528bf987b226bb1ea6ad7
julian gomez <gomezhtml@gmail.com>
2024-02-04 17:56:44 -0500

    Se configura aplicacion para usar JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,24 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports:[
     TypeOrmModule.forFeature([User]),
+    PassportModule.register({defaultStrategy:'jwt'}),
+    JwtModule.register({
+      secret:process.env.JWT_SECRET, //firmar tokens
+      signOptions:{
+        expiresIn:'2h' //que expiren en dos horas
+      }
+    })
   ],
   exports:[TypeOrmModule] //Esto esporta lac onfiguracion TypeOrmModule
+
 })
 export class AuthModule {}
