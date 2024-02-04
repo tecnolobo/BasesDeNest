4f51137325c9edb4e3edb67c8b41c1c419acb0c9
julian gomez <gomezhtml@gmail.com>
2024-02-04 17:22:24 -0500

    Encriptar contraseña y retornar usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -15,13 +16,24 @@ export class AuthService {
     
   }
 
+  //para encriptar contraseña usamos el paquete  yarn add bcrypt
   async create(createUserDto: CreateUserDto) {
     // return 'This action adds a new auth';
     try {
-      const user = this.userRepository.create(createUserDto);
+      const  {password, ...userData} = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password:bcrypt.hashSync(password,10)//encriptamos la contraseña y 10 vueltas
+      });
+
       await this.userRepository.save(user);
 
+
+      delete user.password;//Eliminanos del json la password
+
       return user;
+
     } catch (error) {
       this.handleDBErrors(error);
     }
